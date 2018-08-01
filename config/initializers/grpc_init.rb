48d55364fa6f30b1abced21d7371072efe2e92e6
Shinsuke Nishio <nishio@densan-labs.net>
2018-08-02 02:13:05 +0900

    Autoreload

--- config/initializers/grpc_init.rb
+++ config/initializers/grpc_init.rb
@@ -1,15 +1,33 @@
 # TODO: Auto-reload
-module Grpc
-end
+Rails.application.config.to_prepare do
+  module Grpc
+  end
 
-grpc_libs = "#{Rails.root}/proto/lib/"
-$LOAD_PATH.unshift(grpc_libs)
+  if defined? Ec
+    Google::Protobuf::DescriptorPool.generated_pool.clear()
 
-Dir.glob(grpc_libs + '**/*.rb').each { |f| require f }
+    Ec.class_eval do
+      remove_const(:GetProductsRequest)
+      remove_const(:GetProductsResponse)
+      remove_const(:GetByProductIdRequest)
+      remove_const(:GetByProductIdResponse)
+      remove_const(:Product)
+      remove_const(:Tag)
+      remove_const(:ProductService)
+    end
+  end
 
-grpc_services = "#{Rails.root}/app/grpc/**/*.rb"
-Dir.glob(grpc_services).each { |f| require f }
+  grpc_libs = "#{Rails.root}/proto/lib/"
+  $LOAD_PATH.unshift(grpc_libs)
 
-# TODO: fix dir
-grpc_extends = "#{Rails.root}/app/models/ec/*.rb"
-Dir.glob(grpc_extends).each { |f| require f; puts f }
+  # Dir.glob(grpc_libs + '**/*.rb').each { |f| require f }
+  load grpc_libs + "ec_pb.rb"
+  load grpc_libs + "ec_services_pb.rb"
+
+  # grpc_services = "#{Rails.root}/app/grpc/**/*.rb"
+  # Dir.glob(grpc_services).each { |f| require f }
+  #
+  # # TODO: fix dir
+  # grpc_extends = "#{Rails.root}/app/models/ec/*.rb"
+  # Dir.glob(grpc_extends).each { |f| require f; puts f }
+ end
